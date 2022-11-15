ba7576ff8b841dcc6f99fce399339eb78b8e09d0
Boldizsar Szabo <szboldisz@gmail.com>
2022-11-15 13:35:54 +0100

    Add functionality, only works with two integers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// VARIABLES
+let displaySmall = document.querySelector(".display-small");
+let displayBig = document.querySelector(".display-big");
+
+let numberBtns = document.querySelectorAll(".num");
+let operatorBtns = document.querySelectorAll(".operator");
+
+let operator = "";
+let previousNumber = "";
+let currentNumber = "";
+
+// FUNCTIONS
 function add(a, b) {
   return a + b;
 }
@@ -20,9 +32,38 @@ function operate(operator, num1, num2) {
       return add(num1, num2);
     case "-":
       return subtract(num1, num2);
-    case "*":
+    case "×":
       return multiply(num1, num2);
-    case "/":
+    case "÷":
       return divide(num1, num2);
   }
 }
+
+// EVENT LISTENERS
+numberBtns.forEach((num) => {
+  num.addEventListener("click", (e) => {
+    displayBig.textContent += e.target.textContent;
+    currentNumber = displayBig.textContent;
+  });
+});
+
+operatorBtns.forEach((op) => {
+  op.addEventListener("click", (e) => {
+    if (e.target.textContent !== "=") {
+      operator = e.target.textContent;
+      previousNumber = currentNumber;
+      currentNumber = "";
+      displaySmall.textContent = `${previousNumber} ${operator}`;
+      displayBig.textContent = currentNumber;
+    } else if (e.target.textContent === "=") {
+      displaySmall.textContent += ` ${currentNumber} ${e.target.textContent}`;
+      displayBig.textContent = operate(
+        operator,
+        Number(previousNumber),
+        Number(currentNumber)
+      );
+    } else {
+      displayBig.textContent = "ERROR";
+    }
+  });
+});
